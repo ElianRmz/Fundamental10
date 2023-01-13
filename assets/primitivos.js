005c5ec6142b7cb609d2cffedf07ecf0250f281e
Elian Ramirez <elianramirez@MacBook-Pro-de-Elian.local>
2023-01-12 19:01:21 -0600

    assets

---
+++ assets/primitivos.js
@@ -0,0 +1,14 @@
+let crypto ='bitcoin';
+console.log(crypto);
+crypto = 'etheryum';
+console.log(crypto);
+//string
+crypto = 'XRP';
+console.log( typeof crypto);
+//Numero
+crypto = 1234
+console.log( typeof crypto);
+//boolean
+let iscrypto = true
+console.log( typeof iscrypto);
+//array
